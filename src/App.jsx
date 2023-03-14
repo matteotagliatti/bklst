8edaad0ea1b9d4a45715d8c7114cf636a74c7fc9
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-14 19:01:27 +0100

    fetch books from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
+import books from "./books.json";
+
 function App() {
+  const [booklist, setBooklist] = useState([]);
+
+  useEffect(() => {
+    setBooklist(books);
+    console.log(books);
+  }, []);
+
   return (
-    <div className="relative">
-      <h1>Test</h1>
-    </div>
+    <main className="w-full max-w-xl mx-auto py-20 p-5">
+      <div>
+        <h1 className="mb-1 text-neutral-900">(Basic) Booklist</h1>
+        <p className="text-neutral-500">This is a basic booklist.</p>
+      </div>
+      <div className="mt-10">
+        <p>Booklist</p>
+        <ul>
+          {booklist.map((book) => (
+            <li key={book.id}>
+              <span>{book.title}</span> - <span>{book.author}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
 
