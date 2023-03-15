03029fc9f9e409c480306a1a7725b60ed35ec22b
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-15 16:58:15 +0100

    form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect, useRef } from "react";
+import Form from "./components/Form";
 
 import PocketBase from "pocketbase";
 const pb = new PocketBase("http://127.0.0.1:8090");
 
 function App() {
   const [booklist, setBooklist] = useState([]);
-
-  const titleRef = useRef();
-  const authorRef = useRef();
 
   useEffect(() => {
     getBooklist();
@@ -20,29 +18,13 @@ function App() {
     setBooklist(records);
   }
 
-  async function addBook() {
-    const data = {
-      title: titleRef.current.value,
-      author: authorRef.current.value,
-    };
-    const record = await pb.collection("books").create(data);
-    getBooklist();
-  }
-
   return (
-    <main className="w-full max-w-xl mx-auto py-20 p-5">
+    <main className="w-full max-w-xl mx-auto py-10 md:py-20 p-5">
       <div>
         <h1 className="mb-1 text-neutral-900">(Basic) Booklist</h1>
         <p className="text-neutral-500">This is a basic booklist.</p>
       </div>
-      <div className="mt-10">
-        <p>Insert</p>
-        <form onSubmit={addBook}>
-          <input required type="text" placeholder="Title" ref={titleRef} />
-          <input required type="text" placeholder="Author" ref={authorRef} />
-          <button type="submit">Add</button>
-        </form>
-      </div>
+      <Form pb={pb} />
       <div className="mt-10">
         <p>Booklist</p>
         <ul>
---
+++ src/components/Form.jsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+
+export default function Form({ pb }) {
+  const titleRef = useRef();
+  const authorRef = useRef();
+
+  async function addBook() {
+    const data = {
+      title: titleRef.current.value,
+      author: authorRef.current.value,
+    };
+    const record = await pb.collection("books").create(data);
+  }
+
+  return (
+    <div className="mt-10">
+      <p>Insert</p>
+      <form onSubmit={addBook}>
+        <input required type="text" placeholder="Title" ref={titleRef} />
+        <input required type="text" placeholder="Author" ref={authorRef} />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
