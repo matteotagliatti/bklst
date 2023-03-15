0c872946640c3c85f4407e420e66788a9d7b3510
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-15 23:08:15 +0100

    add created

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,8 +4,11 @@ export default function List({ booklist }) {
       <p>Booklist</p>
       <ul>
         {booklist.map((book) => (
-          <li key={book.id}>
-            <span>{book.title}</span> - <span>{book.author}</span>
+          <li key={book.id} className="flex justify-between">
+            <div>
+              <span>{book.title}</span> - <span>{book.author}</span>
+            </div>
+            <span>{book.created.split("-", 2).join("-")}</span>
           </li>
         ))}
       </ul>
