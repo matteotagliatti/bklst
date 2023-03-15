06800dcf3abb43c8766f8ea3bd1a769caca459ff
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-15 19:46:53 +0100

    get books based on loggged user

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,45 @@
 import { useState, useEffect } from "react";
-import Form from "./components/Form";
-
 import PocketBase from "pocketbase";
+
 import Header from "./components/Header";
+import Create from "./components/Create";
 import List from "./components/List";
+import { LoginLogout } from "./components/LogInLogout";
 const pb = new PocketBase("http://127.0.0.1:8090");
 
 function App() {
   const [booklist, setBooklist] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    getBooklist();
-  }, []);
+    if (user) {
+      getBooklist();
+    }
+  }, [user]);
 
   async function getBooklist() {
     const records = await pb.collection("books").getFullList({
-      sort: "-created",
+      filter: "owner.name = " + '"' + user.record.name + '"',
     });
     setBooklist(records);
   }
 
+  const logUser = () => {
+    console.log(user);
+  };
+
   return (
     <main className="w-full max-w-xl mx-auto py-10 md:py-20 p-5">
+      <button onClick={logUser}>LogUser</button>
       <Header />
-      <Form pb={pb} />
-      <List booklist={booklist} />
+      {user ? (
+        <>
+          <Create pb={pb} getBooklist={getBooklist} />
+          <List booklist={booklist} />
+        </>
+      ) : (
+        <LoginLogout pb={pb} setUser={setUser} />
+      )}
     </main>
   );
 }
---
+++ src/components/LogInLogout.jsx
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+
+export function LoginLogout({ pb, setUser }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function login(event) {
+    const data = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    event.preventDefault();
+    const authData = await pb
+      .collection("users")
+      .authWithPassword(data.email, data.password);
+    setUser(authData);
+  }
+
+  return (
+    <div className="mt-10">
+      <h2>Login</h2>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          placeholder="email"
+          defaultValue={"matteotagliatti@gmail.com"}
+          ref={emailRef}
+        />
+        <input
+          type="password"
+          placeholder="***"
+          defaultValue={"password"}
+          ref={passwordRef}
+        />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
