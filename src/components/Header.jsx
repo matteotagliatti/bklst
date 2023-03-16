3ee750de036e1205e5fb57435ef60112c091085c
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-16 11:28:47 +0100

    dynamic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,25 @@
-export default function Header({ user, pb, setUser }) {
-  const title = "(Basic) Booklist";
-
-  function logout() {
-    localStorage.removeItem("authData");
-    setUser(null);
-    pb.authStore.clear();
-  }
-
+export default function Header({
+  title,
+  subtitle,
+  buttons,
+  user,
+  pb,
+  setUser,
+}) {
   return (
     <header className="flex flex-col md:flex-row flex-start md:justify-between">
       <div>
-        <h1 className="font-medium mb-1 text-neutral-900">
-          {user ? user.record.name + "'s " + title : title}
-        </h1>
-        <p className="text-neutral-500 mb-1">
-          {user ? "Your basic booklist" : "A basic booklist"}
-        </p>
+        <h1 className="font-medium mb-1 text-neutral-900">{title}</h1>
+        <p className="text-neutral-500 mb-1">{subtitle}</p>
       </div>
-
       {user ? (
-        <button className="w-fit" onClick={logout}>
-          Logout
-        </button>
+        <>
+          {buttons.map((buttonInfo, i) => (
+            <button key={i} className="w-fit" onClick={buttonInfo.onClick}>
+              {buttonInfo.text}
+            </button>
+          ))}
+        </>
       ) : null}
     </header>
   );
