134db84cffdb74b423238fe72fc7779721442051
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-20 23:11:07 +0100

    manage no books

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,3 +1,5 @@
+import List from "./List";
+
 export default function Booklist({
   books,
   booksToRead,
@@ -12,56 +14,17 @@ export default function Booklist({
 
           <div className="mt-3">
             <h3 className="text-neutral900 font-medium mb-1">Reading</h3>
-            {booksReading.length === 0 ? (
-              <p>No books found</p>
-            ) : (
-              <>
-                {booksReading.map((book) => (
-                  <a
-                    key={book.id}
-                    href={`/${book.id}`}
-                    className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
-                  >
-                    <div>
-                      <span>{book.title}</span> - <span>{book.author}</span>
-                    </div>
-                    <span>{book.created.split("-", 2).join("-")}</span>
-                  </a>
-                ))}
-              </>
-            )}
+            <List books={booksReading} />
           </div>
 
           <div className="mt-3">
             <h3 className="text-neutral900 font-medium mb-1">Read</h3>
-            {booksRead.map((book) => (
-              <a
-                key={book.id}
-                href={`/${book.id}`}
-                className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
-              >
-                <div>
-                  <span>{book.title}</span> - <span>{book.author}</span>
-                </div>
-                <span>{book.created.split("-", 2).join("-")}</span>
-              </a>
-            ))}
+            <List books={booksRead} />
           </div>
 
           <div className="mt-3">
             <h3 className="text-neutral900 font-medium mb-1">To read</h3>
-            {booksToRead.map((book) => (
-              <a
-                key={book.id}
-                href={`/${book.id}`}
-                className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
-              >
-                <div>
-                  <span>{book.title}</span> - <span>{book.author}</span>
-                </div>
-                <span>{book.created.split("-", 2).join("-")}</span>
-              </a>
-            ))}
+            <List books={booksToRead} />
           </div>
         </section>
       ) : (
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,62 +1,23 @@
-export default function List({ booksToRead, booksReading, booksRead }) {
+export default function List({ books }) {
   return (
     <>
-      {booksToRead.length > 0 &&
-      booksReading.length > 0 &&
-      booksRead.length > 0 ? (
-        <section>
-          <h2 className="text-neutral900 font-medium mb-1">Booklist</h2>
-
-          <div className="mt-3">
-            <h3 className="text-neutral900 font-medium mb-1">Reading</h3>
-            {booksReading.map((book) => (
-              <a
-                key={book.id}
-                href={`/${book.id}`}
-                className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
-              >
-                <div>
-                  <span>{book.title}</span> - <span>{book.author}</span>
-                </div>
-                <span>{book.created.split("-", 2).join("-")}</span>
-              </a>
-            ))}
-          </div>
-
-          <div className="mt-3">
-            <h3 className="text-neutral900 font-medium mb-1">Read</h3>
-            {booksRead.map((book) => (
-              <a
-                key={book.id}
-                href={`/${book.id}`}
-                className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
-              >
-                <div>
-                  <span>{book.title}</span> - <span>{book.author}</span>
-                </div>
-                <span>{book.created.split("-", 2).join("-")}</span>
-              </a>
-            ))}
-          </div>
-
-          <div className="mt-3">
-            <h3 className="text-neutral900 font-medium mb-1">To read</h3>
-            {booksToRead.map((book) => (
-              <a
-                key={book.id}
-                href={`/${book.id}`}
-                className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
-              >
-                <div>
-                  <span>{book.title}</span> - <span>{book.author}</span>
-                </div>
-                <span>{book.created.split("-", 2).join("-")}</span>
-              </a>
-            ))}
-          </div>
-        </section>
+      {books.length === 0 ? (
+        <p>No books found</p>
       ) : (
-        <h2>Loading...</h2>
+        <>
+          {books.map((book) => (
+            <a
+              key={book.id}
+              href={`/${book.id}`}
+              className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
+            >
+              <div>
+                <span>{book.title}</span> - <span>{book.author}</span>
+              </div>
+              <span>{book.created.split("-", 2).join("-")}</span>
+            </a>
+          ))}
+        </>
       )}
     </>
   );
