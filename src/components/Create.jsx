55a6d06ac7d80eae5af6f405ce0e194081ee61c9
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-20 17:46:46 +0100

    add status to create, different layout

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -4,11 +4,13 @@ import { redirect } from "react-router-dom";
 export default function Create({ pb, getBooklist, userID }) {
   const titleRef = useRef();
   const authorRef = useRef();
+  const statusRef = useRef();
 
   async function addBook(event) {
     const data = {
       title: titleRef.current.value,
       author: authorRef.current.value,
+      status: statusRef.current.value,
       owner: userID,
     };
 
@@ -26,10 +28,15 @@ export default function Create({ pb, getBooklist, userID }) {
       <h2 className="text-neutral900 font-medium mb-1">Add book</h2>
       <form
         onSubmit={addBook}
-        className="flex flex-col justify-start gap-y-1 md:flex-row md:justify-between"
+        className="flex flex-col justify-start gap-y-1 md:gap-y-2"
       >
         <input required type="text" placeholder="Title" ref={titleRef} />
         <input required type="text" placeholder="Author" ref={authorRef} />
+        <select className="w-fit" ref={statusRef}>
+          <option value="to-read">To Read</option>
+          <option value="reading">Reading</option>
+          <option value="read">Read</option>
+        </select>
         <input
           className="w-fit hover:cursor-pointer hover:underline"
           type="submit"
