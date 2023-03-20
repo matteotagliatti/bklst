ee0340c28b9e4ce2dc75b2c3856408650de21002
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-20 19:39:01 +0100

    better form

--- src/components/CreateEdit.jsx
+++ src/components/CreateEdit.jsx
@@ -46,13 +46,34 @@ export default function CreateEdit({ pb, userID, book, bookID }) {
         onSubmit={book ? updateBook : addBook}
         className="flex flex-col justify-start gap-y-1 md:gap-y-2"
       >
-        <input required type="text" placeholder="Title" ref={titleRef} />
-        <input required type="text" placeholder="Author" ref={authorRef} />
-        <select className="w-fit" ref={statusRef}>
-          <option value="to-read">To Read</option>
-          <option value="reading">Reading</option>
-          <option value="read">Read</option>
-        </select>
+        <div className="flex gap-2">
+          <label htmlFor="title" className="text-neutral-500">
+            Title:
+          </label>
+          <input
+            id="title"
+            required
+            type="text"
+            placeholder="Title"
+            ref={titleRef}
+          />
+        </div>
+        <div className="flex gap-2">
+          <label htmlFor="author" className="text-neutral-500">
+            Author:
+          </label>
+          <input required type="text" placeholder="Author" ref={authorRef} />
+        </div>
+        <div className="flex gap-2">
+          <label className="text-neutral-500" htmlFor="status">
+            Status:
+          </label>
+          <select className="w-fit" ref={statusRef}>
+            <option value="to-read">To Read</option>
+            <option value="reading">Reading</option>
+            <option value="read">Read</option>
+          </select>
+        </div>
         <input
           className="w-fit hover:cursor-pointer hover:underline"
           type="submit"
