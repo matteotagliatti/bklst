64b5c0109f5251b1d9b983b601ff49297a63b76d
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-21 10:55:52 +0100

    aadd search books via google books api

---
+++ src/components/Search.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useRef } from "react";
+
+export default function Search() {
+  const api_key = import.meta.env.VITE_GOOGLE_API_KEY;
+  const titleRef = useRef();
+  const authorRef = useRef();
+
+  async function fetchBooks(e) {
+    try {
+      e.preventDefault();
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${titleRef.current.value}+inauthor:${authorRef.current.value}&key=${api_key}`
+      );
+      const books = response.data.items;
+      console.log(books);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <section>
+      <h2 className="text-neutral900 font-medium mb-1">Search for a book</h2>
+      <form
+        onSubmit={fetchBooks}
+        className="flex flex-col justify-start gap-y-1 md:gap-y-2"
+      >
+        <div className="flex gap-2">
+          <label htmlFor="title" className="text-neutral-500">
+            Title:
+          </label>
+          <input
+            id="title"
+            required
+            type="text"
+            placeholder="Title"
+            ref={titleRef}
+          />
+        </div>
+        <div className="flex gap-2">
+          <label htmlFor="author" className="text-neutral-500">
+            Author:
+          </label>
+          <input required type="text" placeholder="Author" ref={authorRef} />
+        </div>
+
+        <input
+          className="w-fit hover:cursor-pointer hover:underline"
+          type="submit"
+          value={"Search"}
+        />
+      </form>
+    </section>
+  );
+}
--- src/routes/Add.jsx
+++ src/routes/Add.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import CreateEdit from "../components/CreateEdit";
+import Search from "../components/Search";
 
 export default function Add({ pb, user }) {
   return (
     <>
       <Link to="/">-- back</Link>
+      <Search />
       {user ? <CreateEdit pb={pb} userID={user.record.id} /> : null}
     </>
   );
