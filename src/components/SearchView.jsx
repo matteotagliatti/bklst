60d37023d43410b145dd7ce0f1b01f9761bbb3c4
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-21 17:58:17 +0100

    searchView link

--- src/components/SearchView.jsx
+++ src/components/SearchView.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function SearchView({ searchedBooks }) {
   return (
     <section>
       <h2 className="text-neutral900 font-medium mb-1">Searched books</h2>
       <div className="grid grid-cols-3 gap-2">
-        {searchedBooks.map((book) => {
+        {searchedBooks.map((book, index) => {
           return (
-            <div
+            <Link
+              to={`/add`}
+              state={{ book }}
               className="bg-neutral-100 rounded p-2 hover:shadow-lg hover:cursor-pointer transition-all duration-100"
-              key={book.id}
+              key={book.id + index}
             >
               <p>{book.volumeInfo.title}</p>
               <p>{book.volumeInfo.authors[0]}</p>
-            </div>
+            </Link>
           );
         })}
       </div>
