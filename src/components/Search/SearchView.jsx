e288993ea81eab9255da40d29f31306a621e8497
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-21 19:41:04 +0100

    add img

--- src/components/Search/SearchView.jsx
+++ src/components/Search/SearchView.jsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
 
 export default function SearchView({ searchedBooks }) {
+  console.log(searchedBooks);
+
   return (
     <section>
       <h2 className="text-neutral900 font-medium mb-1">Searched books</h2>
-      <div className="grid grid-cols-3 gap-2">
+      <div className="grid grid-cols-3 gap-5">
         {searchedBooks.map((book, index) => {
+          const title = book.volumeInfo.title;
+          const author = book.volumeInfo.authors[0];
+          const img = book.volumeInfo.imageLinks;
+
           return (
             <Link
               to={`/add`}
               state={{ book }}
-              className="bg-neutral-100 rounded p-2 hover:shadow-lg hover:cursor-pointer transition-all duration-100"
+              className="bg-neutral-100 rounded p-5 hover:shadow-lg hover:cursor-pointer transition-all duration-100 flex flex-col gap-y-1 justify-center items-center"
               key={book.id + index}
             >
-              <p>{book.volumeInfo.title}</p>
-              <p>{book.volumeInfo.authors[0]}</p>
+              <img
+                className="h-28 w-fit object-cover"
+                src={img ? img.thumbnail : ""}
+                alt={title}
+              />
+              <p className="text-center">{title}</p>
+              <p className="text-neutral-500 text-center">{author}</p>
             </Link>
           );
         })}
