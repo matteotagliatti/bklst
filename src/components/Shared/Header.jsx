d48d12991e96201b8c605ee63827c08d30b41a03
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-24 22:18:38 +0100

    fix header

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header({ title, subtitle, button, search }) {
   return (
-    <header className="flex flex-col-reverse gap-y-10 md:flex-row flex-start md:justify-between md:items-center">
+    <header className="flex flex-col-reverse md:flex-row flex-start md:justify-between md:items-center">
       <div>
         <h1 className="font-medium mb-1 text-neutral-900">{title}</h1>
         <p className="text-neutral-500 mb-1">{subtitle}</p>
