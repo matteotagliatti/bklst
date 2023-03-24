ae92d1c6fb6caeb15e734825b4bcb7cbe99b6dc4
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-24 22:15:01 +0100

    sort read book by finished date

--- src/components/Home/List.jsx
+++ src/components/Home/List.jsx
@@ -5,18 +5,38 @@ export default function List({ books }) {
         <p>No books found</p>
       ) : (
         <>
-          {books.map((book) => (
-            <a
-              key={book.id}
-              href={`/book/${book.id}`}
-              className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
-            >
-              <div>
-                <span>{book.title}</span> - <span>{book.author}</span>
-              </div>
-              {/* <span>{book.finished}</span> */}
-            </a>
-          ))}
+          {books
+            // transform the date string into a date object
+            .map((book) => {
+              if (book.finished) {
+                return {
+                  ...book,
+                  finishedDate: new Date(book.finished),
+                };
+              }
+              return book;
+            })
+            // sort the books by the finished date
+            .sort((a, b) => {
+              if (a.finishedDate && b.finishedDate) {
+                return b.finishedDate - a.finishedDate;
+              }
+              return 0;
+            })
+            .map((book) => (
+              <a
+                key={book.id}
+                href={`/book/${book.id}`}
+                className="flex gap-x-2 justify-between flex-wrap mb-1 hover:underline hover:cursor-pointer"
+              >
+                <div>
+                  <span>{book.title}</span> - <span>{book.author}</span>
+                </div>
+                {book.status === "read" ? (
+                  <span>{book.finished.replace(/ .*/, "")}</span>
+                ) : null}
+              </a>
+            ))}
         </>
       )}
     </>
