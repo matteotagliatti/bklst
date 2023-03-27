e126ea1cfe4cae830ede6b2903454eaca3b1ab7e
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-28 01:41:59 +0200

    font to inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.html", "./src/**/*.jsx"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
