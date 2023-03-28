32108db62080e9131637050ef8a4efc41542b9fd
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-28 09:07:55 +0200

    better header

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -8,26 +8,28 @@ export default function Header({ user, setUser, pb }) {
   }
 
   return (
-    <header className="absolute top-0 left-0 right-0 h-12 px-3 py-4 mx-2 flex justify-between items-center border-b border-neutral-100">
-      <Link className="text-sm underline italic" to="/">
-        Index
-      </Link>
+    <header className="fixed top-0 left-0 right-0 h-12 px-3 py-4 mx-2 flex justify-between items-center border-b border-neutral-100">
+      <div className="flex gap-4">
+        <Link className="text-sm underline italic" to="/">
+          Index
+        </Link>
+        {user ? (
+          <Link
+            className="text-sm text-neutral-400 hover:underline"
+            to="/search"
+          >
+            Search
+          </Link>
+        ) : null}
+      </div>
       <nav>
         {user ? (
-          <div className="flex gap-4">
-            <Link
-              className="text-sm text-neutral-400 hover:underline"
-              to="/search"
-            >
-              Search
-            </Link>
-            <button
-              className="text-sm text-neutral-400 hover:underline"
-              onClick={logout}
-            >
-              Logout
-            </button>
-          </div>
+          <button
+            className="text-sm text-neutral-400 hover:underline"
+            onClick={logout}
+          >
+            Logout
+          </button>
         ) : (
           <Link
             className="text-sm text-neutral-400 hover:underline"
