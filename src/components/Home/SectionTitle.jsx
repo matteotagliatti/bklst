bf4502778207b9832e93da887a53d7c1c56553ae
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-28 21:55:45 +0200

    chevron icon fix

--- src/components/Home/SectionTitle.jsx
+++ src/components/Home/SectionTitle.jsx
@@ -3,7 +3,7 @@ export default function SectionTitle({ title, description }) {
     <div className="relative flex flex-col justify-center items-center py-10">
       <h2 className="text-2xl after">{title}</h2>
       <p className="text-sm text-neutral-400">{description}</p>
-      <span className="hidden md:block md:absolute md:right-8 md:top-auto md:bottom-auto text-neutral-400 text-2xl rotate-90 md:rotate-0">
+      <span className="hidden md:block absolute right-4 top-auto bottom-auto text-neutral-400 text-2xl">
         &#8250;
       </span>
     </div>
