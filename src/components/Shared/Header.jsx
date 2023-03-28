457dc7c03ee4145c12e131781a760eaa78a7a61c
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-28 10:30:10 +0200

    header bg and back filter

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -8,7 +8,7 @@ export default function Header({ user, setUser, pb }) {
   }
 
   return (
-    <header className="fixed top-0 left-0 right-0 h-12 px-3 py-4 mx-2 flex justify-between items-center border-b border-neutral-100">
+    <header className="fixed top-0 left-0 right-0 h-12 px-3 py-4 mx-2 flex justify-between items-center border-b border-neutral-100 bg-white/75 backdrop-blur z-10">
       <div className="flex gap-4">
         <Link className="text-sm underline italic" to="/">
           Index
