13bc28b5bfcdec44a944e06f47cc48ef0b5900c0
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-04-05 10:31:27 +0200

    section title no pt on mobile prop

--- src/components/Home/SectionTitle.jsx
+++ src/components/Home/SectionTitle.jsx
@@ -1,6 +1,13 @@
-export default function SectionTitle({ title, description }) {
+export default function SectionTitle({ title, description, noptmobile }) {
+  let classNames = "flex flex-col items-center justify-center";
+  if (noptmobile) {
+    classNames += " pb-6 md:py-6";
+  } else {
+    classNames += " py-6";
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center py-6">
+    <div className={classNames}>
       <h2 className="text-2xl after">{title}</h2>
       <p className="text-sm text-neutral-400">{description}</p>
     </div>
