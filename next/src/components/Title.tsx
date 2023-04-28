4ad6dc0b7133fabb8b79005f6c740ca0457e33e9
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-04-28 11:57:15 +0200

    better title arrow link

--- next/src/components/Title.tsx
+++ next/src/components/Title.tsx
@@ -41,10 +41,23 @@ export default function Title({
         <>
           {bookLength > 3 ? (
             <Link
-              className="group text-sm text-neutral-400 hover:underline"
+              className="group text-sm text-neutral-400 hover:underline flex gap-1 items-center"
               href={link}
             >
-              See more →
+              See more{" "}
+              <svg
+                className="group-hover:-rotate-45 transition-transform duration-200"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z"
+                  fill="currentColor"
+                />
+              </svg>
             </Link>
           ) : null}
         </>
