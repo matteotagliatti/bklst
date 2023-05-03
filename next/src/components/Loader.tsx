398bdbae705009dd4b3c71f840a1a500fb7811ac
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-03 11:26:17 +0200

    better loader

--- next/src/components/Loader.tsx
+++ next/src/components/Loader.tsx
@@ -2,9 +2,9 @@ import LoaderIcon from "./LoaderIcon";
 
 export default function Loader() {
   return (
-    <div className="flex items-center gap-2 text-neutral-400">
+    <div className="flex items-center gap-2">
       <LoaderIcon />
-      loading...
+      <p className="text-sm text-neutral-400">loading...</p>
     </div>
   );
 }
