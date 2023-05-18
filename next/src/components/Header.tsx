a47ca8ba038b895e4a97b783e701a4d76ef2ba87
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-18 13:53:54 +0200

    new nav categories

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -13,56 +13,56 @@ export default function Header() {
   }
 
   return (
-    <header className="fixed top-0 left-0 right-0 h-12 px-3 py-4 mx-2 flex justify-between items-center border-b border-neutral-100 bg-white/75 backdrop-blur z-10">
-      <div className="flex gap-4">
-        <Link className="text-sm underline italic" href="/">
-          Index
-        </Link>
-        {user ? (
-          <>
+    <>
+      <header className="fixed top-0 left-0 right-0 h-12 px-3 py-4 mx-2 flex justify-between items-center border-b border-neutral-100 bg-white/75 backdrop-blur z-10">
+        <div className="flex gap-4">
+          <Link className="text-sm underline italic" href="/">
+            Index
+          </Link>
+          {user ? (
             <Link
               className="text-sm text-neutral-400 hover:underline"
               href="/search"
             >
               Search
             </Link>
-
-            <div className="relative group">
-              <p className="text-sm text-neutral-400 hover:underline cursor-pointer">
-                Categories
-              </p>
-              <div className="text-neutral-400 hidden absolute top-5 left-0 group-hover:flex flex-col gap-2 rounded bg-white p-2 shadow">
-                <Link href={"/reading"} className="text-sm hover:underline">
-                  Reading
-                </Link>
-                <Link href={"/to-read"} className="text-sm hover:underline">
-                  To Read
-                </Link>
-                <Link href={"/read"} className="text-sm hover:underline">
-                  Read
-                </Link>
-              </div>
-            </div>
-          </>
-        ) : null}
-      </div>
-      <nav>
-        {user ? (
-          <button
-            className="text-sm text-neutral-400 hover:underline"
-            onClick={() => logout()}
-          >
-            Logout
-          </button>
-        ) : (
-          <Link
-            className="text-sm text-neutral-400 hover:underline"
-            href="/signin"
-          >
-            Sign In
-          </Link>
-        )}
-      </nav>
-    </header>
+          ) : null}
+        </div>
+        <nav>
+          {user ? (
+            <button
+              className="text-sm text-neutral-400 hover:underline"
+              onClick={() => logout()}
+            >
+              Logout
+            </button>
+          ) : (
+            <Link
+              className="text-sm text-neutral-400 hover:underline"
+              href="/signin"
+            >
+              Sign In
+            </Link>
+          )}
+        </nav>
+      </header>
+      {user ? (
+        <nav className="z-10 fixed bottom-4 md:bottom-7 right-0 left-0 flex justify-center">
+          <div className="flex gap-5 bg-white px-3 py-2 rounded shadow border border-neutral-100 bg-white/75 backdrop-blur z-10">
+            <Link href={"/reading"} className="text-sm">
+              Reading
+            </Link>
+            <div className="w-[1px] h-full bg-neutral-200"></div>
+            <Link href={"/read"} className="text-sm">
+              Read
+            </Link>
+            <div className="w-[1px] h-full bg-neutral-200"></div>
+            <Link href={"/to-read"} className="text-sm">
+              To Read
+            </Link>
+          </div>
+        </nav>
+      ) : null}
+    </>
   );
 }
