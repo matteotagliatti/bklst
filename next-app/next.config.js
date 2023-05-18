3a331711673ffc14b0d9a7c47adf0af7a1803478
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-18 14:04:53 +0200

    rm next app

--- next-app/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["books.google.com"],
-  },
-};
-
-module.exports = nextConfig;
--- next-app/src/app/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Book {
-  id: string;
-  title: string;
-  author: string;
-  img: string;
-  status: string;
-  finished?: string;
-  created_at: string;
-}
