ce5493732536d1f544356cbbd06f8be2a36e3a06
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-18 11:34:33 +0200

    better book on mobile

--- next/src/components/Book.tsx
+++ next/src/components/Book.tsx
@@ -13,24 +13,23 @@ export default function Book({ href, book }: BookProps) {
   }
 
   const classNames =
-    "relative h-96 bg-neutral-100 flex items-center justify-center p-2 rounded-lg hover:bg-[#EEEEF0] transition-colors group";
+    "relative md:h-96 bg-neutral-100 flex gap-5 items-center justify-start md:justify-center p-4 md:p-2 rounded-lg hover:bg-[#EEEEF0] transition-colors group";
   const innerElements = (
     <>
       <Image
-        className="w-32 md:w-36 lg:w-40 group-hover:-translate-y-1 transition-transform ease-in-out shadow-lg drop-shadow-lg"
+        className="w-24 md:w-36 lg:w-40 group-hover:-translate-y-1 transition-transform ease-in-out shadow-lg drop-shadow-lg"
         width={400}
         height={400}
         priority
         src={book.img}
         alt={book.title}
       />
-      <div className="absolute bottom-3 left-3 right-3 flex justify-between items-end">
+      <div className="md:absolute md:bottom-3 md:left-3 md:right-3 flex justify-between items-end">
         <div>
           <small className="block text-neutral-400">{book.author}</small>
-          <p className="text-sm line-clamp-1">{book.title}</p>
+          <p className="text-sm line-clamp-2 md:line-clamp-1">{book.title}</p>
         </div>
-        <div className="text-neutral-400 text-right">
-          <small className="block text-sm">{book.status}</small>
+        <div className="absolute bottom-3 left-3 right-3 md:relative md:bottom-0 md:left-0 md:right-0 text-neutral-400 text-right">
           {book.status === "read" ? (
             <small className="block">{formatDate(book.finished)}</small>
           ) : null}
