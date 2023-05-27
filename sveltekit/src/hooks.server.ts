7e596caf4315ba3d2eec3b0af005dbcbabc4b8d7
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-27 19:43:21 +0200

    protected routes for non logged user

--- sveltekit/src/hooks.server.ts
+++ sveltekit/src/hooks.server.ts
@@ -4,6 +4,7 @@ import {
 } from "$env/static/public";
 import { createSupabaseServerClient } from "@supabase/auth-helpers-sveltekit";
 import type { Handle } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
@@ -12,15 +13,19 @@ export const handle: Handle = async ({ event, resolve }) => {
     event,
   });
 
-  /**
-   * A convenience helper so we can just call await getSession() instead const { data: { session } } = await supabase.auth.getSession()
-   */
   event.locals.getSession = async () => {
     const {
       data: { session },
     } = await event.locals.supabase.auth.getSession();
     return session;
   };
+
+  if (event.url.pathname !== "/signin" && event.url.pathname !== "/") {
+    const session = await event.locals.getSession();
+    if (!session) {
+      throw redirect(303, "/");
+    }
+  }
 
   return resolve(event, {
     filterSerializedResponseHeaders(name) {
