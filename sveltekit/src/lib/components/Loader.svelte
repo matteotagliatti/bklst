a562a025dd10fa67f1277490ae942e2d7ca74a46
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-27 15:17:52 +0200

    add loader

---
+++ sveltekit/src/lib/components/Loader.svelte
@@ -0,0 +1,8 @@
+<script>
+  import LoaderIcon from "./LoaderIcon.svelte";
+</script>
+
+<div class="flex items-center gap-2">
+  <LoaderIcon />
+  <p class="text-sm text-neutral-400">loading...</p>
+</div>
