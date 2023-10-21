6d5875868b0bd2247b056c925dfe65971c671e1d
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-10-21 16:08:56 +0200

    better server get books

--- src/routes/to-read/+page.server.ts
+++ src/routes/to-read/+page.server.ts
@@ -1,18 +1,22 @@
-import type { PageServerLoad } from './$types';
+import type { BookType } from "$lib/types";
+import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
-	const session = await getSession();
+export const load: PageServerLoad = async ({
+  locals: { supabase, getSession },
+}) => {
+  const session = await getSession();
 
-	if (!session) {
-		return;
-	}
+  if (!session) {
+    return;
+  }
 
-	const { data: books } = await supabase
-		.from('books')
-		.select()
-		.eq('owner', session.user.id)
-		.eq('status', 'to-read')
-		.order('updated_at', { ascending: false });
+  const { data } = await supabase
+    .from("books")
+    .select()
+    .eq("owner", session.user.id)
+    .eq("status", "to-read")
+    .order("updated_at", { ascending: false });
 
-	return { books };
+  const books = data as BookType[];
+  return { books };
 };
