3915f6011c63fdb494f96d3b3821288b44d9fb8a
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-10-27 22:36:55 +0200

    disable button when loading

--- src/lib/components/Form/Submit.svelte
+++ src/lib/components/Form/Submit.svelte
@@ -7,6 +7,7 @@
 
 <button
   type="submit"
+  disabled={loading}
   class="flex h-[2.4rem] w-[5.5rem] shrink-0 items-center justify-center gap-2 self-start rounded-md border border-neutral-200 text-sm hover:cursor-pointer hover:border-neutral-300"
 >
   {#if loading}
