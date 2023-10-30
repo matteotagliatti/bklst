7a7089e9819b7c08753eb45917746702ff466219
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-10-30 19:09:24 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import BackIcon from "$lib/components/BackIcon.svelte";
+  import Layout from "$lib/components/Layout.svelte";
+  import Title from "$lib/components/Title.svelte";
+</script>
+
+<Layout>
+  <BackIcon />
+  <Title
+    title={String($page.status)}
+    description={$page.error ? $page.error.message : "Unspecified error"}
+  />
+</Layout>
