dbb474e964fe2c4c3e35ec57347bb61db590c849
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-03 17:03:03 +0100

    switch to ssr auth

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,19 +2,31 @@ import {
   PUBLIC_SUPABASE_ANON_KEY,
   PUBLIC_SUPABASE_URL,
 } from "$env/static/public";
-import type { Database } from "$lib/types/supabase";
-import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit";
+import { createBrowserClient, isBrowser, parse } from "@supabase/ssr";
 import type { LayoutLoad } from "./$types";
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
   depends("supabase:auth");
 
-  const supabase = createSupabaseLoadClient<Database>({
-    supabaseUrl: PUBLIC_SUPABASE_URL,
-    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-    event: { fetch },
-    serverSession: data.session,
-  });
+  const supabase = createBrowserClient(
+    PUBLIC_SUPABASE_URL,
+    PUBLIC_SUPABASE_ANON_KEY,
+    {
+      global: {
+        fetch,
+      },
+      cookies: {
+        get(key) {
+          if (!isBrowser()) {
+            return JSON.stringify(data.session);
+          }
+
+          const cookie = parse(document.cookie);
+          return cookie[key];
+        },
+      },
+    }
+  );
 
   const {
     data: { session },
