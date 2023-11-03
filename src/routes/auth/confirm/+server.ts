2c5d31bf90b2c4f497a752b26ec8ce43153bf1c6
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-03 17:50:01 +0100

    back to prev more generic endpoint

--- src/routes/auth/confirm/+server.ts
+++ src/routes/auth/confirm/+server.ts
@@ -2,11 +2,6 @@ import type { EmailOtpType } from "@supabase/supabase-js";
 import { redirect } from "@sveltejs/kit";
 import type { RequestEvent } from "../../$types";
 
-/**
- *
- * @param event
- * @description API Endoint for confirming email OTP, useful for  Magic Link and Password Recovery
- */
 export const GET = async (event: RequestEvent) => {
   const {
     url,
@@ -14,29 +9,15 @@ export const GET = async (event: RequestEvent) => {
   } = event;
   const token_hash = url.searchParams.get("token_hash") as string;
   const type = (url.searchParams.get("type") as EmailOtpType) || null;
+  const next = url.searchParams.get("next") ?? "/";
 
   if (token_hash && type) {
-    const { data, error } = await supabase.auth.verifyOtp({ token_hash, type });
+    const { error } = await supabase.auth.verifyOtp({ token_hash, type });
     if (!error) {
-      const session = data.session;
-
-      if (!session) {
-        throw new Error("No session");
-      }
-
-      const { data: user, error } = await supabase
-        .from("users")
-        .select()
-        .eq("id", session.user.id)
-        .single();
-
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      throw redirect(303, `/${user.username}`);
+      throw redirect(303, `/${next.slice(1)}`);
     }
   }
 
+  // return the user to an error page with some instructions
   throw new Error("Invalid token");
 };
