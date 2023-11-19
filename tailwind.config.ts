a315c3813cc0bec3037179a5d107aefb5f16b238
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-19 20:03:32 +0100

    better import font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [require("@tailwindcss/forms")],
 };
