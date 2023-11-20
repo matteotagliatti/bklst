28b7cae66b22ec986c11e3a9637aff69f2fef5b2
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-20 23:15:58 +0100

    simpler title

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -4,9 +4,7 @@
   export let mb = "mb-1";
 </script>
 
-<div class="flex max-w-xs flex-row items-end justify-between gap-2 pb-4">
-  <div>
-    <h2 class="text-2xl {mb}">{title}</h2>
-    <p class="text-sm text-neutral-400">{description}</p>
-  </div>
+<div class="max-w-xs pb-4">
+  <h2 class="text-2xl {mb}">{title}</h2>
+  <p class="text-sm text-neutral-400">{description}</p>
 </div>
