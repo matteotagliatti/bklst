5b918df36df69f7306ec3f05e285f89f559e7f41
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-23 16:24:54 +0100

    new heart icon

--- src/lib/components/Form/Hearth.svelte
+++ src/lib/components/Form/Hearth.svelte
@@ -10,23 +10,25 @@
     {disabled}
     {id}
     {name}
-    class="peer relative h-4 w-4 shrink-0 appearance-none border-none bg-transparent text-transparent focus-within:ring-0 focus:outline-none focus:ring-0 focus:ring-offset-0"
+    class="peer relative h-5 w-5 shrink-0 appearance-none border-none bg-transparent text-transparent focus-within:ring-0 focus:outline-none focus:ring-0 focus:ring-offset-0 {disabled
+      ? ''
+      : 'hover:cursor-pointer'}"
     type="checkbox"
     bind:checked
   />
   <svg
-    class="pointer-events-none absolute h-4 w-4 fill-none stroke-red-500 peer-checked:!fill-red-500"
     xmlns="http://www.w3.org/2000/svg"
+    class="pointer-events-none absolute h-5 w-5 fill-none stroke-red-500 peer-checked:!fill-red-500"
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    stroke="currentColor"
     stroke-width="2"
+    stroke="currentColor"
+    fill="none"
     stroke-linecap="round"
     stroke-linejoin="round"
+    ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+      d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"
+    /></svg
   >
-    <path
-      d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-    />
-  </svg>
 </div>
